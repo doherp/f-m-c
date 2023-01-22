780a0bc204b9c662564291000a99e9db4c9932a3
doherp <doherp@gmail.com>
2023-01-22 10:35:22 +0000

    fmc copyover

---
+++ devrun.bat
@@ -0,0 +1,3 @@
+start cmd /k & npm run pdserve
+start cmd "c1"	rem remremremremremremremremremremremremrem	this reqd else 2nd window doesnt run cmd
+start cmd /k cd C:\dev\find-my-course\functions & npm run tunnel 
