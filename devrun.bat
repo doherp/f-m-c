f1207031c574f4eaa819ed764e028f2d404a32b4
doherp <doherp@gmail.com>
2023-01-22 17:24:06 +0000

    devrun bat does firebase and ngrok

--- devrun.bat
+++ devrun.bat
@@ -1,3 +1,4 @@
+rem start cmd /k & npm run pdserve
+rem start cmd "c1"	rem remremremremremremremremremremremremrem	this reqd else 2nd window doesnt run cmd
+start cmd /k npm run tunnel 
 start cmd /k & npm run pdserve
-start cmd "c1"	rem remremremremremremremremremremremremrem	this reqd else 2nd window doesnt run cmd
-start cmd /k cd C:\dev\find-my-course\functions & npm run tunnel 
